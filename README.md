c71dd912cc02e40866a8422ba2b5f1d70290758d
Lukas Celinak <58947427+lukascelinak@users.noreply.github.com>
2022-07-22 08:54:17 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,25 @@
+# User Bulk Action Reset Password #
+Action can reset passsword for selected users, 
+thats means moodle will reset password, 
+moodle will generate new and notify user with new password, 
+after login is user forced to change his password.
+
+## License ##
+
+2022 Lukas Celinak <lukascelinak@gmail.com>
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program.  If not, see <http://www.gnu.org/licenses/>.
+
+If you find this project useful, please consider making a donation.
+
+[![paypal](https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif)](https://paypal.me/lukascelinak?country.x=SK&locale.x=sk_SK)
---
+++ user_bulk_resetpassword.php
@@ -0,0 +1,90 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Script for bulk reset password with create new and send it to user function
+ *
+ * For install, copy file to moodledir/admin/user/
+ *
+ * @package     mod_ratingprompts
+ * @author      Lukas Celinak <lukascelinak@gmail.com>
+ * @copyright   2022 Lukas Celinak, Edumood <lukascelinak@gmail.com>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require_once('../../config.php');
+require_once('lib.php');
+require_once($CFG->libdir.'/adminlib.php');
+
+$confirm = optional_param('confirm', 0, PARAM_BOOL);
+
+admin_externalpage_setup('userbulk');
+require_capability('moodle/user:update', context_system::instance());
+
+$return = $CFG->wwwroot.'/'.$CFG->admin.'/user/user_bulk.php';
+
+if (empty($SESSION->bulk_users)) {
+    redirect($return);
+}
+
+echo $OUTPUT->header();
+
+if ($confirm and confirm_sesskey()) {
+    // only force password change if user may actually change the password
+    $authsavailable = get_enabled_auth_plugins();
+    $changeable = array();
+
+    foreach($authsavailable as $authplugin) {
+        if (!$auth = get_auth_plugin($authplugin)) {
+            continue;
+        }
+        if ($auth->is_internal() and $auth->can_change_password()) {
+            $changeable[$authplugin] = true;
+        }
+    }
+
+    $parts = array_chunk($SESSION->bulk_users, 300);
+    foreach ($parts as $users) {
+        list($in, $params) = $DB->get_in_or_equal($users);
+        $rs = $DB->get_recordset_select('user', "id $in", $params);
+        foreach ($rs as $user) {
+            if (!empty($changeable[$user->auth])) {
+                set_user_preference('create_password', 1, $user->id);
+                set_user_preference('auth_forcepasswordchange', 1, $user->id);
+                unset($SESSION->bulk_users[$user->id]);
+            } else {
+                echo $OUTPUT->notification(get_string('resetpasswordnot', 'bulkusers', fullname($user, true)));
+            }
+        }
+        $rs->close();
+    }
+    echo $OUTPUT->notification(get_string('changessaved'), 'notifysuccess');
+    echo $OUTPUT->continue_button($return);
+
+} else {
+    list($in, $params) = $DB->get_in_or_equal($SESSION->bulk_users);
+    $userlist = $DB->get_records_select_menu('user', "id $in", $params, 'fullname', 'id,'.$DB->sql_fullname().' AS fullname', 0, MAX_BULK_USERS);
+    $usernames = implode(', ', $userlist);
+    if (count($SESSION->bulk_users) > MAX_BULK_USERS) {
+        $usernames .= ', ...';
+    }
+    echo $OUTPUT->heading(get_string('confirmation', 'admin'));
+    $formcontinue = new single_button(new moodle_url('/admin/user/user_bulk_resetpassword.php', array('confirm' => 1)), get_string('yes'));
+    $formcancel = new single_button(new moodle_url('/admin/user/user_bulk.php'), get_string('no'), 'get');
+    echo $OUTPUT->confirm(get_string('resetpasswordcheckfull', 'bulkusers', $usernames), $formcontinue, $formcancel);
+}
+
+echo $OUTPUT->footer();
